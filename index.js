551d2d05c0d578c464bc707f03e54f9a29f2e913
Abdullah Nasir <nm7640785@gmail.com>
2024-03-31 17:26:30 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+"use strict";
+//Question  1
+let moviename = "D Block";
+console.log(moviename);
+//Question 2
+let Myname = "MAbdullah";
+let age = "age 19";
+console.log(`2: ${Myname} ${age}`);
+//Question  3
+const mood = false;
+console.log("3: happy" + "" + mood);
+//Question 4
+let firstname = "Abdullah";
+let lastname = "Nasir";
+console.log(`4: ${firstname} ${lastname}`);
+//Question 5
+let user_age = 12;
+console.log(user_age);
+//Question 6
+let product = "supra";
+let prize = "10000";
+console.log(`6: ${product} ${prize}`);
+//Question 7
+let numberofpets = 3;
+console.log("7: " + "" + numberofpets);
+//Question 8
+const month = false;
+console.log("8: today your birthday" + "" + month);
+//Question 9
+let userDetails = `userDetail:
+firstName: ${firstname}
+lastname: ${lastname}`;
+console.log(userDetails);
+//Question 10
+const birthyear = 2005;
+console.log("birthyear");
+//Question 11
+let city = "Lahore";
+let country = "Pakistan";
+let fullAddress = `Address: ${city}, ${country}`;
+console.log(fullAddress);
+//Question12
+let todaytemperature = 33;
+console.log(todaytemperature);
